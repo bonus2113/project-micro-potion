a09fbedb03b6d1632c10422a1711394168cc00b5
Remy Stuurwold <remystuurwold+sourcetree@gmail.com>
2014-05-24 20:05:05 +0200

    scene update, time manager + timer ui

---
+++ pmip/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+
+    private float m_InternalTimer = 0.0f;
+    private const float TimeToFamilyReturn = 60.0f;
+
+    public UISprite m_TimerSprite = null;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        this.m_TimerSprite.fillAmount = 0.0f;
+        this.m_TimerSprite.fillDirection = UISprite.FillDirection.Radial360;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.m_InternalTimer += Time.deltaTime;
+
+        this.m_TimerSprite.fillDirection = UISprite.FillDirection.Radial360;
+        this.m_TimerSprite.fillAmount = this.m_InternalTimer / TimeToFamilyReturn;
+
+        if (this.m_InternalTimer > TimeToFamilyReturn)
+        {
+            Lose();
+        }
+	}
+
+    private void Lose()
+    {
+        Debug.Log("Lose!");
+    }
+}
