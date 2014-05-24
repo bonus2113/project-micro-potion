b3f5b6cd8bfab1f130065fd13d65a85e1456808d
Remy Stuurwold <remystuurwold+sourcetree@gmail.com>
2014-05-24 20:31:39 +0200

    sound+sound script

---
+++ pmip/Assets/Scripts/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+
+    AudioClip m_Poem;
+
+	// Use this for initialization
+	void Start () {
+
+        AudioSource.PlayClipAtPoint(m_Poem, Camera.main.transform.position);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
