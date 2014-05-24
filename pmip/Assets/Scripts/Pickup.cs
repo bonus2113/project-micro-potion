2ff44ee0f2198abca5d0273086f4747d102a78e9
Dario Seyb <dario.seyb@gmail.com>
2014-05-24 19:20:52 +0200

    pickups

---
+++ pmip/Assets/Scripts/Pickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour 
+{
+    public void Pick(Transform newParent)
+    {
+        transform.parent = newParent;
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.isKinematic = true;
+    }
+
+    public void Throw(Vector3 dir)
+    {
+        transform.parent = null;
+        rigidbody.isKinematic = false;
+        rigidbody.AddForce(dir, ForceMode.Impulse);
+    }
+}
--- pmip/Assets/Scripts/Player/ThrowStuff.cs
+++ pmip/Assets/Scripts/Player/ThrowStuff.cs
@@ -2,6 +2,7 @@
 
 public class ThrowStuff : MonoBehaviour
 {
+    public GameObject CrossHair;
     public KeyCode ThrowKey;
     public GameObject ObjectToThrow;
     public float ThrowStrength = 10;
@@ -10,17 +11,54 @@ public class ThrowStuff : MonoBehaviour
     {
 	    if (Input.GetMouseButtonDown(0))
 	    {
-
-	        ThrowObject();
+	        if (ObjectToThrow == null)
+	        {
+	            PickObject();
+	        }
+	        else
+	        {
+                ThrowObject();
+	        }
 	    }
+
+        var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 3))
+        {
+            CrossHair.transform.position = hit.point;
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Pickup"))
+            {
+                CrossHair.renderer.material.color = Color.green;
+            }
+            else
+            {
+                CrossHair.renderer.material.color = Color.red;
+            }
+        }
+        else
+        {
+            CrossHair.renderer.material.color = Color.red;
+        }
 	}
+
+    void PickObject()
+    {
+        var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 3, 1 << LayerMask.NameToLayer("Pickup")))
+        {
+            ObjectToThrow = hit.collider.gameObject;
+            ObjectToThrow.GetComponent<Pickup>().Pick(Camera.main.transform);
+        }
+    }
 
     void ThrowObject()
     {
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
 
-        var obj = (GameObject)GameObject.Instantiate(ObjectToThrow);
-        obj.transform.position = ray.origin;
-        obj.rigidbody.AddForce(ray.direction * ThrowStrength, ForceMode.Impulse);
+        ObjectToThrow.GetComponent<Pickup>().Throw(ray.direction * ThrowStrength);
+        ObjectToThrow = null;
     }
 }
